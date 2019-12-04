55c9274359f8351300426575fc4c0b9e9d309eec
ahmednour005 <58489887+ahmednour005@users.noreply.github.com>
2019-12-04 09:35:55 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(function () {
  //preloader
       $(" .loading-overlay .loader").fadeOut(2000,function (){
         $("body").css("overflow","auto");
-       $(this).parent().fadeOut(1000,
+       $(this).parent().fadeOut(500,
        function (){
         $(this).remove();
        });	
